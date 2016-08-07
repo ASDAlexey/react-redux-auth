7f55e95305a1bcb40cc7ea38c1359685b335b73d
ASDAlexey <asdalexey@yandex.ru>
2016-08-07 23:49:29 +0300

    change header component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,7 +6,6 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                dsffg
                 <Header />
                 {this.props.children}
             </div>
