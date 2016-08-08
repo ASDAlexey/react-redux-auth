10d52473a3ebedf017c40f1c91bf74cb0df8ec19
Alexey Popov <alexey.popov@umbrella-web.com>
2016-08-08 15:30:10 +0300

    created indexRoute

---
+++ src/components/welcome.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to our slice of paradice.
+            </div>
+        );
+    }
+}
+
+export default Welcome;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { Router, Route, browserHistory } from "react-router";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import reduxThunk from "redux-thunk";
 import App from "./components/app";
-import reducers from "./reducers";
 import Signin from "./components/auth/signin";
 import Signout from "./components/auth/signout";
 import Signup from "./components/auth/signup";
+import Feature from "./components/feature";
+import RequireAuth from "./components/auth/require_authentification";
+import Welcome from "./components/welcome";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
@@ -16,9 +19,11 @@ ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <Router history={browserHistory}>
             <Route path="/" component={App}>
+                <IndexRoute component={Welcome} />
                 <Route path="signin" component={Signin} />
                 <Route path="signout" component={Signout} />
                 <Route path="signup" component={Signup} />
+                <Route path="feature" component={RequireAuth(Feature)} />
             </Route>
         </Router>
     </Provider>
